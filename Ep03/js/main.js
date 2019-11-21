662e1b62ddf2ed5a09ac2556a1746473e2dfde7b
sktama <skodama@sejuku.net>
2019-11-21 16:58:09 +0900

    エラーハンドリング

--- Ep03/js/main.js
+++ Ep03/js/main.js
@@ -1,16 +1,16 @@
 var app = new Vue({
   el: '#app',
   data: {
-    bpi: null
+    bpi: null,
+    hasError: false
   },
   mounted() {
     axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .then((res) => {
-        this.$data.bpi = res.data.bpi
-        console.log(this.$data.bpi)
+        this.bpi = res.data.bpi
       })
       .catch((err) => {
-        console.log(err)
+        this.hasError = true
       })
   },
   filters: {
