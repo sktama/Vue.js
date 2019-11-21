512bbb617884dd5c0107e46c041634fd1800fa0f
sktama <skodama@sejuku.net>
2019-11-21 16:02:44 +0900

    APIからデータ取得ロジック実装

--- Ep03/js/main.js
+++ Ep03/js/main.js
@@ -1,6 +1,17 @@
 var app = new Vue({
-  el: 'app',
+  el: '#app',
   data: {
     bpi: null
+  },
+  mounted() {
+    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then((res) => {
+        this.$data.bpi = res.data.bpi
+        console.log(this.$data.bpi)
+      })
+      // .then(function(res) {
+      //   this.bpi = res.data.bpi
+      //   console.log(this.bpi)
+      // }.bind(this))
   }
 })
