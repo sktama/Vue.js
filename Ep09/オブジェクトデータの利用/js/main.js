f9fe8e5045d011333202e5936993f0a93adc61b9
sktama <skodama@sejuku.net>
2019-11-26 11:51:01 +0900

    クラスとスタイルのバインディング

---
+++ Ep09/オブジェクトデータの利用/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    classObject: {
+      large: true,
+      'text-danger': false
+    }
+  },
+})
---
+++ Ep09/配列構文によるクラスバインディング/js/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    largeClass: 'large',
+    dangerClass: 'text-danger'
+  },
+})
